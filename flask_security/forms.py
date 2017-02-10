52129e21fee5d2d4f53f3dd8eba6b9c7e545efbd
jxltom <jxltom@gmail.com>
2017-02-10 10:01:38 +0800

    Add validator to default LoginForm

--- flask_security/forms.py
+++ flask_security/forms.py
@@ -203,8 +203,10 @@ class PasswordlessLoginForm(Form, UserEmailFormMixin):
 class LoginForm(Form, NextFormMixin):
     """The default login form"""
 
-    email = StringField(get_form_field_label('email'))
-    password = PasswordField(get_form_field_label('password'))
+    email = StringField(get_form_field_label('email'),
+                        validators=[email_required, email_validator])
+    password = PasswordField(get_form_field_label('password'),
+                             validators=[password_required])
     remember = BooleanField(get_form_field_label('remember_me'))
     submit = SubmitField(get_form_field_label('login'))
 
